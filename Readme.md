733eea0927c200663bbba5ff55a8595bfa4ba94a
João Silva <jsilva@thinkdigital.pt>
2022-08-24 12:16:36 +0100

    first release

---
+++ Readme.md
@@ -0,0 +1,32 @@
+
+# Serial
+
+## Tool
+
+  STM32CubeIDE
+
+  Version: 1.4.2
+  Build: 7643_20200813_1322 (UTC)
+
+  (C) 2020 STMicroelectronics ALL RIGHTS RESERVED
+
+## Purpose
+
+  This library is integrated in a bigger project and is now being shared to help you going faster through your project.
+
+  It has been used with STM32L476 but should be compatible with any version of STM32L4xx
+
+## Dependencies
+
+  None
+
+## Functionalities
+
+  Multiple uart support
+  Write and read using interrupts
+  Dedicated rx buffer to store received data
+  rx buffer size can be changed editing RXBUFFERSIZE macro
+
+## Examples
+
+  Besides testing.cpp file, you can check logging, rs485 and modem packages to see more examples
---
+++ testing.cpp
@@ -0,0 +1,132 @@
+
+  /*
+    Pseudo code
+  */
+
+  // AUTO GENERATED
+  static void MX_USART2_UART_Init(void);
+  static void MX_USART1_UART_Init(void);
+  static void MX_USART3_UART_Init(void);
+
+  UART_HandleTypeDef huart1;
+  UART_HandleTypeDef huart2;
+  UART_HandleTypeDef huart3;
+
+  Serial *serial_debug = NULL;
+
+void main(){
+
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+
+  serial_debug = new Serial();
+  serial_debug->begin(uart,baudrate);
+  serial_debug->write((char *)data, size);
+}
+
+// From this point code was AUTO GENERATED using STM32CubeIDE
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler(__FILE__, __LINE__);
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler(__FILE__, __LINE__);
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler(__FILE__, __LINE__);
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
